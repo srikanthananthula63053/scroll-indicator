c8241e507c0558c2e59c7c1baa00c0f8d68e8520
srikanthananthula63053 <sreekanthananthula5@gmail.com>
2024-12-09 12:34:52 +0530

    setting the scroll height using addEventListener and document

--- src/components/index.jsx
+++ src/components/index.jsx
@@ -5,7 +5,8 @@ import { useState } from "react";
 export default function ScrollIndicator({ url }) {
     const [loading, setLoading] = useState(false);
     const [data, setData] = useState([]);
-    const [erorrMessage, setErrorMessage] = useState('')
+    const [erorrMessage, setErrorMessage] = useState('');
+    const [scrollPercentage, setScrollPercentage] = useState(0);
 
     async function fetchData(geturl) {
         try {
@@ -13,20 +14,47 @@ export default function ScrollIndicator({ url }) {
             const response = await fetch(geturl);
             const data = await response.json();
             console.log(data);
+
+            if (data && data.products && data.products.length > 0) {
+                setData(data.products);
+                setLoading(false)
+            }
         } catch (e) {
             console.log(e);
-            console.log()
-
+            console.log();
 
         }
 
     }
 
-    useEffect(()=>{
+    useEffect(() => {
         fetchData(url);
     }, [url]);
+    function handleScrollPercentage() {
+        console.log(document.body.scrollTop, document.documentElement.scrollTop, 
+            document.documentElement.scrollHeight, document.documentElement.clientHeight);
+            
+
+    }
+
+    useEffect(() => {
+        window.addEventListener('scroll', handleScrollPercentage)
+
+        return () => {
+            window.removeEventListener('scroll', () => { })
+        }
+
+    }, []);
+    console.log(data, loading);
     return (
         <div>
+            <h1>coustom scroll Indicator</h1>
+            <div>
+                {
+                    data && data.length > 0 ? data.map((dataItem) =>
+                        <p>{dataItem.title}</p>) : null
+                }
+            </div>
 
         </div>
     );
